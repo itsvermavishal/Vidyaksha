e1667d08b1691d23203eab6bf6fbc7a3c5a6c62f
Vishal Verma <vishalpbh022@gmail.com>
2025-07-21 07:28:03 +0530

    modifications

--- app/src/main/java/com/example/vidyaksha/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/vidyaksha/presentation/dashboard/DashboardScreen.kt
@@ -1,20 +1,34 @@
 package com.example.vidyaksha.presentation.dashboard
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.example.vidyaksha.R
+import com.example.vidyaksha.domain.model.Subject
 import com.example.vidyaksha.presentation.components.CountCard
 
 @Composable
@@ -75,6 +89,46 @@ private fun CountCardSelection(
                 modifier = Modifier.weight(1f),
                 headingText = "Goal Study Hours",
                 count = goalStudyHours
+            )
+        }
+    }
+}
+
+@Composable
+private fun SubjectCardSelection(
+    modifier: Modifier,
+    subjectList: List<Subject>
+){
+    Column(modifier = modifier){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(
+                text = "Subject",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(start = 12.dp)
+            )
+            IconButton(onClick = {/*TODO*/}) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Subject"
+                )
+            }
+        }
+        if (subjectList.isEmpty()){
+            Image(
+                modifier = Modifier.size(120.dp).align(Alignment.CenterHorizontally),
+                painter = painterResource(R.drawable.img_books),
+                contentDescription = ""
+            )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "",
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Gray,
+                textAlign = TextAlign.Center
             )
         }
     }
