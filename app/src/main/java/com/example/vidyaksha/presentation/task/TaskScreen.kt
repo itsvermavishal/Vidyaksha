00b1b46098aa726d6e8ac4f4ab5657a3e5a16cde
Vishal Verma <vishalpbh022@gmail.com>
2025-08-27 07:30:17 +0530

    Completed TaskScreen and Added date Picker

--- app/src/main/java/com/example/vidyaksha/presentation/task/TaskScreen.kt
+++ app/src/main/java/com/example/vidyaksha/presentation/task/TaskScreen.kt
@@ -1,15 +1,27 @@
 package com.example.vidyaksha.presentation.task
 
 import android.widget.Space
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,20 +30,31 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.vidyaksha.presentation.components.DeleteDialog
 import com.example.vidyaksha.presentation.components.TaskCheckBox
+import com.example.vidyaksha.presentation.components.TaskDatePicker
 import com.example.vidyaksha.presentation.theme.Red
+import com.example.vidyaksha.util.Priority
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskScreen(){
+
+    var isDeleteDialogOpen by rememberSaveable { mutableStateOf(false) }
+
+    var isDatePickerDialogOpen by rememberSaveable { mutableStateOf(false) }
+    val datePickerState = rememberDatePickerState()
 
     var title by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
@@ -44,6 +67,26 @@ fun TaskScreen(){
         else -> null
     }
 
+    DeleteDialog(
+        isOpen = isDeleteDialogOpen,
+        title = "Delete Task?",
+        bodyText = "Are you sure, you want to delete this task?" +
+        "This action can not be undone.",
+        onDismissRequest = {isDeleteDialogOpen = false},
+        onConfirmButtonClick = {
+            isDeleteDialogOpen = false
+        }
+    )
+
+    TaskDatePicker(
+        state = datePickerState,
+        isOpen = isDatePickerDialogOpen,
+        onDismissRequest = { isDatePickerDialogOpen = false},
+        onConfirmuttonClicked = {
+            isDatePickerDialogOpen = false
+        }
+    )
+
     Scaffold (
         topBar = {
             TaskScreenTopBar(
@@ -51,13 +94,15 @@ fun TaskScreen(){
                 isComplete = false,
                 checkBoxBorderColor = Red,
                 onBackButtonClick = {},
-                onDeleteButtonClick = {},
+                onDeleteButtonClick = { isDeleteDialogOpen = true},
                 onCheckBoxClick = {}
             )
         }
     ){paddingValue ->
         Column (
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .verticalScroll(state = rememberScrollState())
+                .fillMaxSize()
                 .padding(paddingValue)
                 .padding(horizontal = 12.dp)
         ){
@@ -77,8 +122,80 @@ fun TaskScreen(){
                 onValueChange = { description = it },
                 label = {Text(text = "Description")},
             )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "Due Date",
+                style = MaterialTheme.typography.bodySmall
+            )
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = "27 Aug 2025",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                IconButton(onClick = {isDatePickerDialogOpen = true}){
+                    Icon(
+                        imageVector = Icons.Default.DateRange,
+                        contentDescription = "Select Due Date"
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "Priority",
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Row (modifier = Modifier.fillMaxWidth()){
+                Priority.entries.forEach { priority ->
+                    PriorityButton(
+                        modifier = Modifier.weight(1f),
+                        label = priority.title,
+                        backgroundColor = priority.color,
+                        borderColor = if (priority == Priority.MEDIUM){
+                            Color.White
+                        } else Color.Transparent,
+                        labelColor = if (priority == Priority.MEDIUM){
+                            Color.White
+                        }else Color.White.copy(alpha = 0.7f),
+                        onClick = {}
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(
+                text = "Related to subject",
+                style = MaterialTheme.typography.bodySmall
+            )
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = "English",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                IconButton(onClick = {/*TODO*/}){
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Select subject"
+                    )
+                }
+            }
+            Button(
+                enabled = taskTitleError == null,
+                onClick = {/*TODO*/},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp)
+            ) {
+                Text(text = "Save")
+            }
         }
-
     }
 }
 
@@ -121,4 +238,26 @@ private fun TaskScreenTopBar(
 
         }
     )
+}
+
+@Composable
+private  fun PriorityButton(
+    modifier: Modifier = Modifier,
+    label: String,
+    backgroundColor: Color,
+    borderColor: Color,
+    labelColor: Color,
+    onClick: () -> Unit
+){
+    Box(
+        modifier = modifier
+            .background(backgroundColor)
+            .clickable{onClick()}
+            .padding(5.dp)
+            .border(1.dp, borderColor, RoundedCornerShape(5.dp))
+            .padding(5.dp),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = label, color = labelColor)
+    }
 }
