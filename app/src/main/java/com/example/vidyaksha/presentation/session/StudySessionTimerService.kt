edc946708444b54ff30ef1c01c6f0af8e973b87f
Vishal Verma <vishalpbh022@gmail.com>
2025-09-08 06:50:55 +0530

    Added Update functionalities

--- app/src/main/java/com/example/vidyaksha/presentation/session/StudySessionTimerService.kt
+++ app/src/main/java/com/example/vidyaksha/presentation/session/StudySessionTimerService.kt
@@ -1,4 +1,15 @@
 package com.example.vidyaksha.presentation.session
 
-class StudySessionTimerService {
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class StudySessionTimerService : Service(){
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
 }
